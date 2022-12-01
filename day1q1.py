a9807365002822016fdc698e838003698328f80a
categoraal <76538493+categoraal@users.noreply.github.com>
2022-12-01 23:09:43 +0100

    Day1 Q1 & Q2

---
+++ day1q1.py
@@ -0,0 +1,21 @@
+input = open("input1.txt","r")
+#print(type(input))
+
+input = input.read() #string input
+lists = input.split('\n\n') #lists
+
+b = [0]*len(lists)
+for i in range(len(lists)-1):
+    var = lists[i].split('\n')
+    array = [0]*len(var)
+    #print(array)
+    for j in range(len(var)):
+        array[j] = int(var[j])
+    #print(array)
+    b[i] = sum(array)
+
+b.sort(reverse=True)
+
+listlength = len(b)
+result = b[0]+b[1]+b[2]
+print(b[0], result)
