55ee268ecfac9ab5610f3680567b078cbd6e3f04
categoraal <76538493+categoraal@users.noreply.github.com>
2022-12-05 09:43:16 +0100

    day5
    beunnnn

---
+++ day5/day5q1.py
@@ -0,0 +1,40 @@
+input = open('day5/input5.txt').read()
+
+stacks, moves = input.split('\n\n')
+
+stacks = stacks.split('\n')
+
+rows =[]
+columns =[]
+for j in range(9):
+    columns = []
+    for i in range(len(stacks)-1):
+        if stacks[i][1+4*j] != ' ':
+            columns += stacks[i][1+4*j]
+    rows.append(columns)
+
+moves = moves.replace('move ','').replace('from ','').replace('to ','').replace('\n',' ').split(' ')
+moves = [eval(i) for i in moves]
+
+
+for i in range(len(rows)):
+    rows[i] = list(reversed(rows[i]))
+
+
+for i in range(int(len(moves)/3)):
+    crates = moves[3*i]
+    a = int(moves[3*i+1])-1
+    b = int(moves[3*i+2])-1
+    for j in range(int(crates)):
+        z = rows[a].pop()
+        rows[b].append(z)
+
+#for i in range(9):
+#    print(rows[i].pop())
+
+res = []
+for i in range(9):
+    res += rows[i].pop()
+
+res = ''.join(res)
+print(res)
