0c414bbe10da7fa1281b0ed9b1fc9b3068f23bd4
categoraal <76538493+categoraal@users.noreply.github.com>
2022-12-09 14:27:51 +0100

    Update day9q2.py

--- day9/day9q2.py
+++ day9/day9q2.py
@@ -7,7 +7,7 @@ for i in range(len(input)):
     input[i][1] = int(input[i][1])
 
 rope = [[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0],[0,0]] #locatin tail
-#print('first rope:',rope)
+
 
 def isTouching(head, tail):
     res = False
@@ -30,14 +30,10 @@ def movement(head, tail):
         right = -1
     tail[0] += right
     tail[1] += up
-    print(rl, ud)
     return tail
 
 posTail = []
 for i in range(len(input)):
-    oldrope = copy.deepcopy(rope)
-    print('rope befor instruction',rope)
-    print(input[i])
     for j in range(input[i][1]):
         if input[i][0] == 'U':
             rope[0][1] += 1
@@ -47,23 +43,12 @@ for i in range(len(input)):
             rope[0][0] += -1        
         if input[i][0] == 'R':
             rope[0][0] += 1
-#        print('head has moved, tail has not',rope)
-#        print(oldrope)
         for k in range(len(rope)-1):
- #           print(k)
             if isTouching(rope[k],rope[k+1]) == False:
                 movement(rope[k],rope[k+1])
-#                print('head',rope[k],'tail',rope[k+1])
-#                rope[k+1] = oldrope[k]
-#                print('new',rope[k+1],'from oldrope',oldrope[k])
-#                print('rope',rope)
-#                print('oldrope',oldrope)
-#                print(rope)
-    #print(rope)
         posTail = posTail + rope[9]
-print(rope)
+
 posTail = [str(i) for i in posTail]
-print(posTail)
 result = ['0 0']
 for i in range(int(len(posTail)/2)):
     result.append(posTail[2*i] + ' ' + posTail[2*i+1])
