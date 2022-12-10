7f3aab86c4f902ca50e7a3d6a19f39970881c62f
categoraal <76538493+categoraal@users.noreply.github.com>
2022-12-10 09:11:16 +0100

    cleaning up day 10

--- day10/day10q1.py
+++ day10/day10q1.py
@@ -1,7 +1,6 @@
 input = open('day10/input10.txt').read().split('\n')
 input = [row.split(' ') for row in input]
 
-
 def valueX(input):
     instruction = [1, 1]
     for i in range(len(input)):
@@ -13,6 +12,5 @@ def valueX(input):
     return instruction
 
 X = valueX(input)
-
 result = 20*X[20] + 60*X[60] +  100*X[100] + 140*X[140] + 180*X[180] + 220*X[220]
 print(result)
--- day10/day10q2.py
+++ day10/day10q2.py
@@ -4,27 +4,20 @@ input = [row.split(' ') for row in input]
 def valueX(input):
     instruction = [1]
     for i in range(len(input)):
-        if input[i][0] == 'noop':
-            instruction.append(instruction[-1]) 
+        instruction.append(instruction[-1]) 
         if input[i][0] != 'noop':
-            instruction.append(instruction[-1]) 
             instruction.append(instruction[-1] + int(input[i][1]))
     return instruction
 
-X = valueX(input)
-print(X)
-
-def drawScreen():
+def drawScreen(X):
     screenstring = ''
     for i in range(6):
         for j in range(40):
             if abs(j-X[j+i*40]) <= 1:
-                print(j-X[j+i*40])
                 screenstring += '#'
             else:
                 screenstring += '.'
-            #do nothing
         screenstring += '\n'
     print(screenstring)
 
-drawScreen()
+drawScreen(valueX(input))
