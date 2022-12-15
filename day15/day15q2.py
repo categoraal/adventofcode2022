7217063335f6d8cb4e44693b42d6639be258773b
categoraal <76538493+categoraal@users.noreply.github.com>
2022-12-15 14:49:19 +0100

    day 15

--- day15/day15q2.py
+++ day15/day15q2.py
@@ -3,9 +3,9 @@
 #  v down +y
 
 import re
-input = open('day15/input15test.txt').read().strip().split('\n')
+input = open('day15/input15.txt').read().strip().split('\n')
 input = [[int(j) for j in re.findall(r'[\-]\d+|\d+',i)] for i in input]
-rowToCheck = 20
+rowToCheck = 4000000
 
 #print(input)
 
@@ -13,6 +13,9 @@ inputT = list(zip(*input))
 xmax, xmin = max(max(inputT[0]),max(inputT[2])),min(min(inputT[0]),min(inputT[2]))
 ymax, ymin = max(max(inputT[1]),max(inputT[3])),min(min(inputT[1]),min(inputT[3]))
 #print(xmin,xmax,ymin,ymax)
+for i in input:
+    i.append(abs(i[0]-i[2]) + abs(i[1]-i[3]))
+[print(i) for i in input]
 
 for i in input:
     distance = abs(i[0]-i[2]) + abs(i[1]-i[3])
@@ -31,58 +34,36 @@ for i in input:
 print(xmin,xmax,ymin,ymax)
 print(xmax-xmin,ymax-ymin)
 
+def checkpoint(x,y):
+    for i in input:
+        distance = abs(x-i[0])+abs(y-i[1])
+        if distance <= i[4]:
+            return 0
+    return 1
 
-rowy = []
-for i in range(xmax-xmin):
-    rowy += '.'
-#print(rowy)
-rowx = rowy.copy()
-row = []
-for i in input:
-    #print(i)
-    distance = abs(i[0]-i[2]) + abs(i[1]-i[3])
-    #print('distance between beacons',distance)
-    up = i[1] - distance
-    right = i[0] + distance
-    down = i[1] + distance
-    left = i[0] - distance
-    diff = abs(i[1] - rowToCheck)
-    #print('distance to row from beacon',diff)
-    if diff <= distance:
-        xmov = distance-diff
-        row.append(i[0]-xmin-xmov)
-        row.append(i[0]-xmin+xmov+1)
-        
-        
-        
-        for j in range(2*xmov+1):
-        #    #print(j)
-            rowy[i[0]-xmin-xmov+j] = '#'
-        #    #print(i[0]-xmin-xmov+j)
+def checkboundary(x):
+    res = 0
+    #traverse the border:
+    sx, sy, bx, by, d = x
+    dist = abs(sx-bx) + abs(sy-by)
+    cx,cy = sx,sy-d-1
+    for i in [[1,1],[-1,1],[-1,-1],[1,-1]]:
+        for j in range(d+1):
+            if cx <= rowToCheck and cx >= 0 and cy >= 0 and cy <= rowToCheck: #in boundary, check xy-s_k < d-s_k
+                res = checkpoint(cx, cy)
+                if res == 1:
+                    print('coords',cx,cy)
+                    print(cx*4000000+cy)
+                    return 1
+            cx += i[0]
+            cy += i[1]
+    #print('s',sx, sy,'b',bx,by,'dist', dist)
 
-print(row)
+    return 0
 
-for i in range(int(len(row)/2)):
-    #print('diff',row[i*2]-row[i*2+1])
-    for j in range(row[i*2+1]-row[i*2]):
-        #print(j+row[i*2],j)
-        rowx[j+row[i*2]] = '#'
 
 
 for i in input:
-    #if i[1] == rowToCheck:
-    #    rowy[i[0]-xmin] = 'S'
-    if i[3] == rowToCheck:
-        rowy[i[2]-xmin] = 'B'
-        rowx[i[2]-xmin] = 'B'
-
-#print(rowy)
-
-#print(rowx)
-sum = 0
-for i in rowx:
-    if i == '#':
-        sum += 1
-print(len(rowy))
-
-print(sum)
+    res = checkboundary(i)
+    if res == '1':
+        break
